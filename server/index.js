0b788fec7e0d937ee3bf6e0ad4abd9b9a98f2dbd
JK1320 <kaurjus1@gmail.com>
2024-02-21 13:15:30 +0000

    test3-EC2

--- server/index.js
+++ server/index.js
@@ -81,5 +81,5 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("server has started on port 5000 and testing github actions");
+  console.log("server has started on port 5000 and testing Github Actions");
 });
