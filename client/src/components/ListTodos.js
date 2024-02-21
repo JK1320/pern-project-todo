803335a66a48b06d274247a86f481208bfb6d482
JK1320 <kaurjus1@gmail.com>
2024-02-21 14:14:05 +0000

    test s3

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -45,7 +45,7 @@ const ListTodos = () => {
       <table className="table mt-5 text-center">
         <thead>
           <tr>
-            <th>Description</th>
+            <th>Description (test)</th>
             <th>Edit</th>
             <th>Delete</th>
           </tr>
